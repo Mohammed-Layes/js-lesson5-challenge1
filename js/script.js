a22fcb0b6d6c834523bf729314d14eb64113e3f8
Mohammed.Layes <mohammed.layes@gmail.com>
2023-03-13 21:45:19 -0400

    Show / hide image, change button style

--- js/script.js
+++ js/script.js
@@ -5,5 +5,16 @@ var cat = document.querySelector("figure.cat");
 
 button.addEventListener("click", function () {
     // console.log("It's working!!!");
-    cat.classList.add("show");
+    // cat.classList.add("show");
+    if (cat.classList.contains("show")) {
+        // console.log("Yes");
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back!"
+        button.classList.add("disappear");
+    } else {
+        // console.log("No");
+        cat.classList.add("show");
+        button.innerText = "Shoo, cat!!";
+        button.classList.remove("disappear");
+    }
 });
